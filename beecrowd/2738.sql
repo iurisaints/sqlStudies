a0b88b53a3e20b1cf48f0f88519ff30d5b851f85
Iuri Santos <50501178+iurisaints@users.noreply.github.com>
2023-10-16 17:07:20 -0300

    Update 2738.sql

--- beecrowd/2738.sql
+++ beecrowd/2738.sql
@@ -1,7 +1,7 @@
 SELECT candidate.name, 
-((score.math*2 + score.specific*3 + score.project_plan*5)/10) AS 'avg'
+       CAST((((score.math * 2) + (score.specific * 3) + (score.project_plan * 5)) / 10) AS NUMERIC(15, 2)) AS avg
 FROM score
 JOIN candidate ON candidate.id = score.candidate_id
-ORDER BY 'avg' ASC;
+ORDER BY avg DESC;
 
---acho que é assim
+--mt triste essa
