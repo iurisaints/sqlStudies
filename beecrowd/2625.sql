0fb51ba4fef1d69968a580360280db3160e45848
Iuri Santos <50501178+iurisaints@users.noreply.github.com>
2023-10-16 12:31:07 -0300

    Update 2625.sql

--- beecrowd/2625.sql
+++ beecrowd/2625.sql
@@ -1,7 +1,3 @@
-SELECT natural_person.cpf,
-CONCAT(SUBSTRING(natural_person.cpf,1,3),'.',SUBSTRING(natural_person.cpf,4,3),'.',SUBSTRING(natural_person.cpf,7,3),'-',SUBSTRING(natural_person.cpf,10,2)) AS CPF
+SELECT CONCAT(SUBSTRING(cpf,1,3),'.',SUBSTRING(cpf,4,3),'.',SUBSTRING(cpf,7,3),'-',SUBSTRING(cpf,10,2)) AS CPF
 FROM natural_person
-JOIN customers ON customers.id = natural_person.id_customers
-WHERE LENGTH(natural_person.cpf ) = 11
-
--- NÃO ESTÁ FUNCIONAL --
+WHERE LENGTH(cpf) = 11
